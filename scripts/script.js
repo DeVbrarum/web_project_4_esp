6358506786afcb033b2159d2168d6c5332beaec1
DeVbrarum <killi0589@gmail.com>
2023-08-22 14:39:18 -0400

    fix images javascript

--- scripts/script.js
+++ scripts/script.js
@@ -170,7 +170,7 @@ function setPopupImgAction(btn) {
 
 //carga las imagenes preestablecidas por default al website
 let photosDefault = [
-    { name: 'Lago Di Braies', link: '../../images/pictures/lago-di-braies.png' },
+    { name: 'Lago Di Braies', link: './images/pictures/lago-di-braies.png' },
     { name: 'Vrchlabi Czechia', link: '../images/pictures/Vrchlabi Czechia.jpg' },
     { name: 'Latemar', link: '../images/pictures/latemar.png' },
     { name: 'Monte Calvas', link: '../images/pictures/monte-calvas.png' },
